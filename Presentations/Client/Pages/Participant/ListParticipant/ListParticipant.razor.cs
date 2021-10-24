39cec78e42aecabb4482960547a2cf4851bf7733
csw <cahya.sulianto@gmail.com>
2021-10-24 21:16:59 +0700

    participan

---
+++ Presentations/Client/Pages/Participant/ListParticipant/ListParticipant.razor.cs
@@ -0,0 +1,24 @@
+﻿using Client.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Client.Pages.Participant.ListParticipant
+{
+    public partial class ListParticipant : ComponentBase
+    {
+        [Inject] private IJSRuntime JS { get; set; }
+        private IJSObjectReference _jsModule;
+
+        protected ElementReference _dtRef;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                // load JS
+                _jsModule = await JS.ReadJsFile<ListParticipant>();
+                await _jsModule.InvokeVoidAsync("DatatableLoad", _dtRef);
+            }
+        }
+    }
+}
---
+++ Presentations/Client/Pages/Participant/ListParticipant/ListParticipant.razor.js
@@ -0,0 +1,32 @@
+﻿
+export function DatatableLoad(element) {
+
+    if (!$().DataTable) {
+        console.warn('Warning - datatables.min.js is not loaded.');
+        return;
+    }
+
+    // Setting datatable defaults
+    $.extend($.fn.dataTable.defaults, {
+        autoWidth: false,
+        columnDefs: [{
+            orderable: false,
+            width: 100,
+            targets: [5]
+        }],
+        dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
+        language: {
+            search: '<span>Filter:</span> _INPUT_',
+            searchPlaceholder: 'Type to filter...',
+            lengthMenu: '<span>Show:</span> _MENU_',
+            paginate: { 'first': 'First', 'last': 'Last', 'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;' }
+        }
+    });
+
+    // Apply custom style to select
+    $.extend($.fn.dataTableExt.oStdClasses, {
+        "sLengthSelect": "custom-select"
+    });
+
+	$(element).dataTable();
+}
