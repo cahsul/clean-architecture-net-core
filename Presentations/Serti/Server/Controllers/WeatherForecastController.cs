6aacb02cfdb3fd7a22f87a1636d82b58efb395e0
csw <cahya.sulianto@gmail.com>
2021-11-17 16:22:40 +0700

    serti WITH AUTH

--- Presentations/Serti/Server/Controllers/WeatherForecastController.cs
+++ Presentations/Serti/Server/Controllers/WeatherForecastController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Serti.Shared;
 
 namespace Serti.Server.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
